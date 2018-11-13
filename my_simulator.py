5b36946d99a65df821ffc492a908ff0d2597f5e3
ianwfinch <ianawfinch@gmail.com>
2018-11-13 17:04:28 -0500

    added words.txt, typecasting in simulator

--- my_simulator.py
+++ my_simulator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
+import sys
 
 #opens the specified file
-sim = open("simulator.txt", "r")
+args = sys.argv
+sim = open(args[1], "r")
 
 #sets number of possible states, strips leading characters
 numStates = sim.readline().rstrip('\n')
 numStates = numStates[18:]
 numStates = int(numStates)
-print(numStates)
+#print(f'number of states: {numStates}')
 
-#sets number of accepted states, read as a dict (unordered list)
+#sets number of accepted states, read as a list
 accStates = sim.readline().rstrip('\n')
 accStates = accStates[18:]
 accStates = accStates.split(' ')
-print(accStates)
+accStates = map(int, accStates)
+#print(f'accepted states: {accStates}')
 
 #sets alphabet of characters, read as a string
 alphabet = sim.readline().rstrip('\n')
 alphabet = alphabet[10:]
-print(alphabet)
+#print(f'alphabet: {alphabet}')
 
 #add a node for each new line in the file
 #must add a head for each character in alphabet
@@ -31,9 +34,11 @@ for i in range(numStates):
     thisNode = []
     j = 1
     for j in range(len(alphabet)):
-        thisNode.append(foo[j])
+        thisNode.append(int(foo[j]))
     nodeList.append(thisNode)
 
-print(nodeList)
+#print(f'table: {nodeList}')
+sim.close()
         
 
+#inputfile = open(args[2], "r")
